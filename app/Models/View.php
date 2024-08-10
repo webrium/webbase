436dc8a5e9de4fd364fb3c86bb97b0bfb1eec66f
parsgit <nvioir@gmail.com>
2024-08-10 13:52:45 +0000

    add: new function

--- app/Models/View.php
+++ app/Models/View.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use Foxdb\Model;
 use Foxdb\Schema;
+use webrium\Http;
 
 class View extends Model{
 
@@ -24,5 +25,25 @@ class View extends Model{
     $table->create();
   }
 
+
+  public function new($type, $external_id){
+    $ip = Http::ip();
+
+    $view = View::where('external_id', $external_id)->and('ip', $ip)->and('type', $type)->find();
+
+    if($view==false){
+      $view = new View;
+      $view->external_id = $external_id;
+      $view->ip = $ip;
+      $view->type = $type;
+    }
+    else{
+      $view->count += 1;
+    }
+    
+    $view->save();
+
+    return $view;
+  }
 
 }
